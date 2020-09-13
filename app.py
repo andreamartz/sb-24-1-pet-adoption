1463874eed5aed50a3cc31da5f7018518dcde5ab
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-09-13 17:23:46 -0500

    Create route and view function for adding a pet

--- app.py
+++ app.py
@@ -22,3 +22,32 @@ def show_pets():
 
     return render_template('index.html', pets=pets)
 
+
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """Pet add form; handle adding"""
+
+    form = PetForm()
+
+    # check if it's a post request AND validate the token
+    if form.validate_on_submit():
+        # Because of this validate_on_submit(), we can get the data directly from the form object
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        # create the new pet
+        new_pet = Pet(name=name, species=species,
+                      photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Added new pet: {name}")
+        return redirect("/")
+
+    else:
+        return render_template("pet_add_form.html", form=form)
+
